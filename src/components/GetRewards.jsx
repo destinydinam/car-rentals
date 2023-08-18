bb3df6422b78d0a72550234b120597f8eb313b4b
destinydinam <destinydinam1@gmail.com>
2023-08-18 01:59:02 +0000

    adding favicon

--- src/components/GetRewards.jsx
+++ src/components/GetRewards.jsx
@@ -1,5 +1,3 @@
-import { Icons } from "./Icons";
-
 export function GetRewards() {
   return (
     <section className="h-[26rem]">
@@ -10,19 +8,9 @@ export function GetRewards() {
           </h2>
           <p className="text-lg leading-9 mt-6">
             Enroll in our premium spare parts loyalty program! Signing up is
-            swift and simple – download our app and establish your account. As a
-            valued member, relish exclusive advantages and privileges each time
-            you acquire parts from us
+            swift and simple – Just contact us to relish exclusive advantages
+            and privileges each time you acquire parts from us
           </p>
-          <div className="mt-8 flex justify-center">
-            <a
-              aria-label="Download on the App Store"
-              className="rounded-lg transition-colors bg-white text-gray-900 hover:bg-gray-50"
-              href="/#"
-            >
-              <Icons.AppStore />
-            </a>
-          </div>
         </div>
       </div>
     </section>
